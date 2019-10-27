5cde19dc3304229f1b508bdfb473103c25fb28c8
João Gonçalves <jsvgoncalves@gmail.com>
2019-10-27 20:07:32 +0100

    Refactor code
    * Refactors the code
    * The --off switch now works properly with the --id.
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- lights.py
+++ lights.py
@@ -3,29 +3,38 @@ import argparse
 import configparser
 from phue import Bridge
 
-cfg = configparser.ConfigParser()
-cfg.read('config.cfg')
-ip = cfg['Bridge']['ip']
+def set_light(id, state):
+    b.set_light(id, 'on', state)
+
+def main(args):
+    global b
+    b = Bridge(args.ip)
+    if args.id:
+        lid = int(args.id)
+        if args.off:
+            set_light(lid, False)
+        else:
+            set_light(lid, not b.get_light(lid, 'on'))
+
+    elif args.off:
+        set_light(1, False)
+    else:
+        set_light(1, not b.get_light(1, 'on'))
+
 
 if __name__ == '__main__':
+    cfg = configparser.ConfigParser()
+    cfg.read('config.cfg')
+    ip = cfg['Bridge']['ip']
+
     parser = argparse.ArgumentParser(description='Manage phue lights.')
     parser.add_argument('--off',
                         help='set it on or off',
                         action='store_true')
-
     parser.add_argument('--id',
                         help='id of the light to handle')
-
     parser.add_argument('--ip',
                         help='IP of the bridge',
                         default=ip)
-    args = parser.parse_args()
+    main(parser.parse_args())
 
-    b = Bridge(args.ip)
-    if args.id:
-        lid = int(args.id)
-        b.set_light(lid, 'on', not b.get_light(lid, 'on'))
-    elif args.off:
-        b.set_light(1, 'on', False)
-    else:
-        b.set_light(1, 'on', not b.get_light(1, 'on'))
